c8a83d870af41acff717ade2a567689687742999
prasant Ssharma <punkersyt@gmail.com>
2024-06-09 07:09:16 +0545

    hooking

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,14 @@
+import { useState } from "react"; 
+
  
+
 const Header=()=>{
+  const [searchTXt,setSearchTXt]=useState("Hungry? Food or Drinks?");
     return(
       <div className="header">
      <img alt="logo" src="https://trademarks.justia.com/media/og_image.php?serial=73308054"/>
-    <input type="text" placeholder="Search.." className="searchh"/>
+    <input type="text" placeholder="Search.." className="searchh" value={searchTXt} onChange={(e)=>setSearchTXt(e.target.value)}/>
+    <button/>
       <ul className="lists">
      <li>Offers</li>
      <li>Sign In</li>
